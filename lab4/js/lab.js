19e4335aad2dbda52d77979df422a084788491e9
Jjuare <102751764+Jjuare@users.noreply.github.com>
2022-04-18 21:53:25 -0700

    Lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,30 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author Josue Juarez.
+// @since  4.18.2022
+//Rock-Paper-Scissors
+//Print the options for the game
+  //1 - Rock
+  //2 - Paper
+  //3 - Scissors
+//Ask users input for which one they would like to pick
+  //Verify that the input is correct, otherwise loop back to 2
+//Bot
+  //Randomly picks a number between 1-3
+    //1 - rock
+    //2 - paper
+    //3 - scissors
+//Print the winner is based on who won with these parameters
+  //Rock
+    //Rock beats paper
+  //Paper
+    //Paper beats rock
+  //Scissors
+    //Scissors beats paper
+  //Draw - both input value are the same
+    //Loop back to 2
+//Ask the user if they want to replay
+  //If yes loop back to 1
+  //If no end the game
